bc00d20c05d4ed33e7d84ba1d500401c87363678
prince <pdwamena@nthc.com.gh>
2020-08-18 16:09:03 +0000

    done with the spotify playbar

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,47 @@
 import React from "react";
 import "./Footer.css";
+import luda from "./luda.jpg";
+
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import Grid from "@material-ui/core/Grid";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import Slider from "@material-ui/core/Slider";
 
 function Footer() {
   return (
     <div className="footer">
-      <h1>i am foooter</h1>
+      <div className="footer_left">
+        <img className="footer_albumlogo" src={luda} alt="" />
+        <div className="footer_sonyInfo">
+          <h4>Yooo</h4>
+          <p>CrisLuda</p>
+        </div>
+      </div>
+      <div className="footer_center">
+        <ShuffleIcon className="footer_green" />
+        <SkipPreviousIcon className="footer_icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer_green" />
+        <SkipNextIcon className="footer_icon" />
+        <RepeatIcon className="footer_green" />
+      </div>
+      <div className="footer_right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
